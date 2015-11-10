da1031e52c5719083474a2fb0ac9b246f9007a9a
Juha Komulainen <juha.komulainen@evident.fi>
2015-11-10 09:08:04 +0200

    Simplify the example code
    Simply returning empty array will trigger the problem.

--- src/main/java/bug/Main.java
+++ src/main/java/bug/Main.java
@@ -22,9 +22,8 @@ public class Main {
 
         Main main = new Main(args[0], args[1], args[2]);
 
-        main.prepareDatabase();
-        main.separateConnections();
-        main.singleConnection();
+        main.separateConnections(); // looping in separate connections work
+        main.singleConnection(); // looping using single connection fails
     }
 
     private Connection openConnection() throws SQLException {
@@ -37,7 +36,7 @@ public class Main {
         try (Connection connection = openConnection()) {
             for (int i = 0; i < 10; i++) {
                 try {
-                    findDepartmentInfos(connection);
+                    selectEmptyArray(connection);
                 } catch (Exception e) {
                     System.out.println("failed after " + i + " iterations");
                     e.printStackTrace();
@@ -55,7 +54,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             try {
                 try (Connection connection = openConnection()) {
-                    findDepartmentInfos(connection);
+                    selectEmptyArray(connection);
                 }
             } catch (Exception e) {
                 System.out.println("failed after " + i + " iterations");
@@ -68,23 +67,8 @@ public class Main {
         System.out.println("ok");
     }
 
-    private void prepareDatabase() throws SQLException {
-        try (Connection connection = openConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                statement.execute("DROP TABLE IF EXISTS employee");
-                statement.execute("DROP TABLE IF EXISTS department");
-
-                statement.execute("CREATE TABLE department (id INT PRIMARY KEY)");
-                statement.execute("CREATE TABLE employee (id INT PRIMARY KEY, department_id INT NOT NULL REFERENCES department)");
-
-                statement.execute("INSERT INTO department (id) VALUES (1)");
-            }
-        }
-    }
-
-    private void findDepartmentInfos(Connection connection) throws SQLException {
-        try (PreparedStatement ps = connection.prepareStatement(
-                "SELECT array_remove(array_agg(e.id), NULL) FROM department d LEFT JOIN employee AS e ON d.id = e.department_id GROUP BY d.id")) {
+    private void selectEmptyArray(Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT '{}'::int[]")) {
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     try (ResultSet ars = rs.getArray(1).getResultSet()) {
